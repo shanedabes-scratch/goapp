b9cf178dab8ca82de80ca5ee44e34d6bfa463499
shanedabes <shane@donohoe.cc>
2020-05-21 22:16:06 +0100

    pointless addition

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	
+	"fmt"
+
 	"github.com/shanedabes/goapp/cmd"
 )
 
 func main() {
+	fmt.Println("pointless addition")
 
-    
-    cmd.Execute()
-	
+	cmd.Execute()
+
 }
